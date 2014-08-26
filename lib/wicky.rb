a75b27e58bf0d02b569e103c151a1c470c4fda32
Tomoki UDA <tuda@kmc.gr.jp>
2014-08-27 06:40:39 +0900

    shows #{proj.name} etc. in index.haml

--- lib/wicky.rb
+++ lib/wicky.rb
@@ -5,10 +5,14 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'haml'
 require 'tilt/haml'
+require_relative '../models/user'
+require_relative '../models/project'
+require_relative '../models/participation'
+require_relative '../models/schedule'
 
 set :views, "#{File.dirname(__FILE__)}/../views"
 
 get '/' do
-  haml :index
+  haml :index, locals: { projects: Project.all }
 end
 
