0c6e6e0cf4a57a590ff3d12c61514cffb1f8906a
Tomoki UDA <tuda@kmc.gr.jp>
2014-08-28 00:18:21 +0900

    organizes APIs

--- lib/wicky.rb
+++ lib/wicky.rb
@@ -38,16 +38,21 @@ get '/api/projects/:id.json' do |id|
   json Project.find(id).to_json
 end
 
-put '/api/projects/:id' do |id|
+post '/api/projects/' do |id|
   project_data = {
     name: params[:name],
     summary: params[:summary]
   }
-  if (Project.exists?(id)) then
-    json Project.update(id, project_data).to_json
-  else
-    json Project.create(project_data).to_json
-  end
+  json Project.update(project_data).to_json
+end
+
+put '/api/projects/:id' do |id|
+  halt 404 unless Project.exists?(id)
+  project_data = {
+    name: params[:name],
+    summary: params[:summary]
+  }
+  json Project.update(id, project_data).to_json
 end
 
 get '/api/users.json' do
@@ -64,19 +69,17 @@ post '/api/users/' do
     name: params[:name],
     email: params[:email]
   }
+  halt 409 if User.exists?(user_data)
   json User.create(user_data).to_json
 end
 
 put '/api/users/:id' do |id|
+  halt 404 unless User.exists?(id)
   user_data = {
     name: params[:name],
     email: params[:email]
   }
-  if (User.exists?(id)) then
-    json User.update(id, user_data).to_json
-  else
-    json User.create(user_data).to_json
-  end
+  json User.update(id, user_data).to_json
 end
 
 post '/api/participations/' do
@@ -89,10 +92,9 @@ post '/api/participations/' do
   project_id = params[:project_id]
   halt 404 unless Project.exists?(project_id)
   project = Project.find(project_id)
-  unless Participation.exists?(user_id: user_id, project_id: project_id) then
-    project.users.push user
-    project.save
-  end
+  halt 409 if Participation.exists?(user_id: user_id, project_id: project_id)
+  project.users.push user
+  project.save
   json({
     project_id: project_id,
     user_id: user_id,
