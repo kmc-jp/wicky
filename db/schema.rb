08f725fcae5e6f72e10811a89847f6fd48ad8eae
Tomoki UDA <tuda@kmc.gr.jp>
2014-08-28 04:55:11 +0900

    adds schedule.description

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140826214448) do
+ActiveRecord::Schema.define(version: 20140827192553) do
 
   create_table "participations", id: false, force: true do |t|
     t.integer "user_id",    null: false
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20140826214448) do
     t.integer  "project_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   create_table "users", force: true do |t|
