04a536666a931da34666e0d50d734fb7038bdb06
Tomoki UDA <tuda@kmc.gr.jp>
2014-08-27 23:27:33 +0900

    implements participation

--- lib/wicky.rb
+++ lib/wicky.rb
@@ -50,3 +50,52 @@ put '/api/projects/:id' do |id|
   end
 end
 
+get '/api/users.json' do
+  json User.all
+end
+
+get '/api/users/:id.json' do |id|
+  halt 404 unless User.exists?(id)
+  json User.find(id).to_json
+end
+
+post '/api/users/' do
+  user_data = {
+    name: params[:name],
+    email: params[:email]
+  }
+  json User.create(user_data).to_json
+end
+
+put '/api/users/:id' do |id|
+  user_data = {
+    name: params[:name],
+    email: params[:email]
+  }
+  if (User.exists?(id)) then
+    json User.update(id, user_data).to_json
+  else
+    json User.create(user_data).to_json
+  end
+end
+
+post '/api/participations/' do
+  user_data = {
+    name: params[:name],
+    email: params[:email]
+  }
+  user = User.find_or_create_by(user_data)
+  user_id = user.id
+  project_id = params[:project_id]
+  halt 404 unless Project.exists?(project_id)
+  project = Project.find(project_id)
+  unless Participation.exists?(user_id: user_id, project_id: project_id) then
+    project.users.push user
+    project.save
+  end
+  json({
+    project_id: project_id,
+    user_id: user_id,
+  })
+end
+
